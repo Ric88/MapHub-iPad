a1d776ad818907b7bd62cd523bf92ebdbadbb967
Ric <Ric@Ruokuns-MacBook-Air.local>
2011-10-10 20:55:33 -0400

    Initial Commit

---
+++ MapHub/MapHubAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MapHubAppDelegate.h
+//  MapHub
+//
+//  Created by Ruokun Ren on 10/10/11.
+//  Copyright 2011 Cornell University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapHubAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ MapHub/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MapHub
+//
+//  Created by Ruokun Ren on 10/10/11.
+//  Copyright 2011 Cornell University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
